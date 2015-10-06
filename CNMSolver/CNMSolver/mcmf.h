4212b99a025f114d72aaff3993cfa934fe55f0b1
Sanghyeok Park <pps789@naver.com>
2015-10-07 06:57:04 +0900

    implemented MCMF

--- CNMSolver/CNMSolver/mcmf.h
+++ CNMSolver/CNMSolver/mcmf.h
@@ -1,4 +1,8 @@
 #include<vector>
+#include<algorithm>
+#include<queue>
+#include<functional>
+#include<climits>
 using namespace std;
 
 #ifndef _MCMF_H_
@@ -10,14 +14,19 @@ private:
 		int target;
 		int cost;
 		int capacity;
+		int rev; // g[target][rev]
 	};
 	int n;
 	vector<vector<edge> > g;
 
+	vector<int> pi, distance, min_capacity;
+	vector<int> back;
+	bool dijkstra(int from, int to);
+
 public:
 	MCMF(int _n);
 	void add_edge(int from, int to, int cost, int capacity);
-	int mcmf(int from, int to);
+	int mcmf(int source, int sink);
 };
 
 #endif
